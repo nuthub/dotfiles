c81088e204496303bfc4c7bc15bd2b9bd2887abe
Julian Flake <flake@uni-koblenz.de>
2020-03-11 11:14:21 +0100

    add cpu_usage blocklet

---
+++ i3blocks/cpu_usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+idle=$(mpstat 1 1 | tail -n1 | awk '{ print $NF }')
+usage=$(echo "100-$idle" | bc)
+
+# 1st row: full text
+echo " $usage %"
+
+# 2nd row: short text
+echo " $usage %"
+
+# 3rd row: color
+usageInt=$(echo $usage | sed 's/\./ /' | awk '{ print $1 }')
+[ ${usageInt} -ge 15 ] && echo "#FF8000"
+
+exit 0
+
