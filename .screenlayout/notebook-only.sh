b4f694910e915730371e30efcc4ddfa7fba32912
Julian Flake <flake@uni-koblenz.de>
2022-10-04 11:47:02 +0200

    aligned XRandR layout scripts

--- .screenlayout/notebook-only.sh
+++ .screenlayout/notebook-only.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-xrandr --output DP1 --off
-xrandr --output DP2 --off
-xrandr --output DP2-1 --off
-xrandr --output DP2-2 --off
-xrandr --output DP2-3 --off
-xrandr --output HDMI1 --off
-xrandr --output HDMI2 --off
-xrandr --output VIRTUAL1 --off
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off
