64645eb03ab73b453a82cc14f2afaff6601b4f72
Julian Flake <flake@uni-koblenz.de>
2023-12-05 10:23:21 +0100

    revisitied straight bootstrap process

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,37 +4,43 @@
 (unless noninteractive
   (message "Loading %s..." load-file-name))
 
-;; Use straight.el instead of package.el
+;; This is part of the bootstrap sequence of straight.el (from straight.el's "Getting Started": https://github.com/radian-software/straight.el#getting-started)
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;; load use-package.el
-(straight-use-package 'use-package)
-;; and use straight.el's version of use-package by default
-(setq straight-use-package-by-default t)
-
-;; Load org-mode init file
+;; load org via straight.el as early as possible
 (straight-use-package 'org)
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+
+;; Load my literate config
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
 
 (provide 'init)
 ;;; init.el ends here
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "13a8eaddb003fd0d561096e11e1a91b029d3c9d64554f8e897b2513dbf14b277" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default)))
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
