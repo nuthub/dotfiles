7290dd03c77523c0b0a5440e42f7c630cab54ca2
Julian Flake <flake@uni-koblenz.de>
2024-04-09 16:28:06 +0200

    removed obsolete shells and configs

--- .local/bin/guix-shell-astah.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export _JAVA_AWT_WM_NONREPARENTING=1
-guix shell icedtea@3.19.0 \
-     -- /home/flake/Applications/astah_professional/astah -nojvchk
--- .local/bin/guix-shell-java17.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export JAVA_HOME=$(guix build openjdk@17.0.5 | grep "\-jdk$")
-guix shell openjdk@17.0.5:jdk
