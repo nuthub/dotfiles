27edbb0891bb588a1f57e099c8f9797a9c201e9d
Julian Flake <flake@uni-koblenz.de>
2024-07-28 09:46:02 +0200

    calendar sync update, make use of khalel

---
+++ .local/bin/sync-calendars.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# to test the script for mcron, use a --pure shell, e.g.:
+# > guix shell --pure bash vdirsyncer emacs grep gawk -- ~/.local/bin/sync-calendars.sh
+
+export SSL_CERT_DIR=/run/current-system/profile/etc/ssl/certs
+export XDG_RUNTIME_DIR=/run/user/1000
+
+echo "NOW RUNNING vdirsyncer discover"
+vdirsyncer discover
+echo "NOW RUNNING vdirsyncer metasync"
+vdirsyncer metasync
+echo "NOW RUNNING vdirsyncer sync"
+vdirsyncer sync
+echo "NOW RUNNING (khalel-import-events)"
+emacs --init-directory=~/minimal-khalel  --batch --script minimal-khalel/init.el -e 'khalel-import-events'
+echo "NOW RUNNING (jf/revert-file-visiting-buffer)"
+emacsclient -e '(jf/revert-file-visiting-buffer "~/org/calendar.org")'
