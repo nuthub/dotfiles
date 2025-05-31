217059f0aae958cf6fd1c2cd434d36d79a5c7f0b
Julian Flake <flake@uni-koblenz.de>
2025-05-31 21:06:09 +0200

    astah: updated to 10.1.

--- .config/guix/packages/astah.scm
+++ .config/guix/packages/astah.scm
@@ -14,6 +14,11 @@
 ;; Simply setting the license file via Astah's GUI doesn't work, because $GUIX_PROFILE/lib/astah_professional directory (aka astah install directory) is not writable.
 ;; Therefore, the license file needs to be packaged, in order to be able to place it in the $GUIX_PROFILE/lib/astah_professional
 ;; During build time, the license file needs to be found at file:/tmp/astah_professional_license.xml (it must not ne shared with the public)
+;;
+;; There is a more elegant way: see help-guix@gnu.org, 2024-11-13 09:34, Parnikkapore, Re: Add external data to a package
+;; just define an origin (e.g. (define astah-license (origin...)) and use it as (data #astah-license) in the let expression of the 'add-license phase in the astah-professional-licensed package
+;;
+;; Another way is described here: https://www.reddit.com/r/GUIX/comments/1h2zl4a/question_how_do_i_build_a_text_file_and_refer_to/
 (define-public astah-professional-license
   (package
    (name "astah-professional-license")
@@ -35,7 +40,7 @@
 (package
 (inherit astah-professional)
 (name "astah-professional-licensed")
-(version "10.0.0.a1b9b1")
+(version "10.1.0.9ceee1")
 (inputs `(("gcc:lib" ,gcc "lib")
 	  ("astah-professional-license" ,astah-professional-license)))
 (arguments
@@ -46,13 +51,14 @@
    '(("usr/lib/" "lib")
      ("usr/bin/" "bin")
      ("usr/share/" "share"))
-   #:phases (modify-phases %standard-phases
-			   (add-after 'install 'add-license
-				      (lambda* (#:key inputs #:allow-other-keys)
-					(let* ((lic-file "astah_professional_license.xml")
-					       (lib-dir "/lib/astah_professional/"))
-					  (copy-file
-					   (string-append (assoc-ref inputs "astah-professional-license") lib-dir lic-file)
-					   (string-append (assoc-ref %outputs "out") lib-dir lic-file)))
-					#t)))))
+   #:phases (modify-phases
+	     %standard-phases
+	     (add-after 'install 'add-license
+			(lambda* (#:key inputs #:allow-other-keys)
+			  (let* ((lic-file "astah_professional_license.xml")
+				 (lib-dir "/lib/astah_professional/"))
+			    (copy-file
+			     (string-append (assoc-ref inputs "astah-professional-license") lib-dir lic-file)
+			     (string-append (assoc-ref %outputs "out") lib-dir lic-file)))
+			  #t)))))
 (synopsis "Astah Professional (with license file), an Easy-to-use UML2.x modeler")))
