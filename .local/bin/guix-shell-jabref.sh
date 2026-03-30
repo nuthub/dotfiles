84da8f109243659ada98fad0251c31a7cbc6657f
Julian Flake <flake@uni-koblenz.de>
2026-03-30 08:09:08 +0200

    .local/bin: obsolete scripts removed.

--- .local/bin/guix-shell-jabref.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-guix shell \
-     --container --emulate-fhs --network \
-     --preserve='^DBUS_' --expose=/var/run/dbus \
-     --preserve='^XDG_|^WAYLAND_DISPLAY$' --expose=/run/user \
-     --preserve='^DISPLAY$' --expose=/dev/dri --expose=/sys/dev --expose=/sys/devices \
-     --share=$HOME \
-     coreutils gtk+ openjdk@17.0.5 xdg-utils xdg-user-dirs ungoogled-chromium \
-     -- ~/opt/JabRef-5.11/bin/JabRef
