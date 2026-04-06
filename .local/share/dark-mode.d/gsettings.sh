3881282d1054f1e348c662bc1edd42fc82da2886
Julian Flake <flake@uni-koblenz.de>
2026-04-06 19:13:10 +0200

    gtk: switch back to adwaita again.

--- .local/share/dark-mode.d/gsettings.sh
+++ .local/share/dark-mode.d/gsettings.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-#gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark'
-gsettings set org.gnome.desktop.interface gtk-theme 'Orchis-Dark'
+
+# dark / light mode
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' # 'Orchis-Dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 
+# valid in light and dark
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
+gsettings set org.gnome.desktop.interface document-font-name='Noto Serif 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code 11'
--- .local/share/light-mode.d/gsettings.sh
+++ .local/share/light-mode.d/gsettings.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-#gsettings set org.gnome.desktop.interface gtk-theme 'Materia'
-gsettings set org.gnome.desktop.interface gtk-theme 'Orchis'
+
+# dark / light mode
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita' # 'Orchis'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Light'
+
+# valid in light and dark
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
+gsettings set org.gnome.desktop.interface document-font-name='Noto Serif 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code 11'
