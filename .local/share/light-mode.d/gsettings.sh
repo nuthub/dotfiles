8163c759db1c6cf10afb1abfa8e22d5cff9dd2bd
Julian Flake <flake@uni-koblenz.de>
2026-07-24 23:26:44 +0200

    darkman: error in gsettings scripts fixed.

--- .local/share/light-mode.d/gsettings.sh
+++ .local/share/light-mode.d/gsettings.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # dark / light mode
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita' # 'Orchis'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Light'
 
 # valid in light and dark
 gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
-gsettings set org.gnome.desktop.interface document-font-name='Noto Serif 11'
+gsettings set org.gnome.desktop.interface document-font-name 'Noto Serif 11'
 gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code 11'
